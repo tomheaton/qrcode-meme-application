4316ca734eb1b67e13fd876eee95d42f2d954cef
Thomas Heaton <thomas.m.heaton@gmail.com>
2021-09-30 15:13:11 +0100

    fix bug

--- client-next/src/pages/api/data.ts
+++ client-next/src/pages/api/data.ts
@@ -2,7 +2,6 @@ import type {NextApiRequest, NextApiResponse} from 'next';
 import {data} from "../../data";
 
 const handler = (req: NextApiRequest, res: NextApiResponse) => {
-    data.selected = 1
     res.status(200).json(data);
 }
 
--- client-next/src/pages/api/qrcode/[username].tsx
+++ client-next/src/pages/api/qrcode/[username].tsx
@@ -2,11 +2,15 @@ import type {NextApiRequest, NextApiResponse} from 'next';
 import {data} from "../../../data";
 
 const handler = (req: NextApiRequest, res: NextApiResponse) => {
+    // TODO: please connect to database before this gets worse :peepoSad:
     //const { slug } = req.query as { slug: string };
-    //const selectedId = data.selected;
-    // const selected = data.memes.filter((o) => o.id === selectedId)
-    // res.redirect(selected[0].link);
-    res.redirect(data.memes[Math.floor(Math.random() * data.memes.length)].link);
+    if (data.mode==="selected") {
+        const selectedId = data.selected;
+        const selected = data.memes.filter((o) => o.id === selectedId)
+        res.redirect(selected[0].link);
+    } else {
+        res.redirect(data.memes[Math.floor(Math.random() * data.memes.length)].link);
+    }
 }
 
 export default handler;
