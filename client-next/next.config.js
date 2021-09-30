419a1317fc998bd0ebd99ebedf395582aed94992
Thomas Heaton <thomas.m.heaton@gmail.com>
2021-09-30 01:19:34 +0100

    added redirect (for t-shirt)

--- client-next/next.config.js
+++ client-next/next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+
+  async redirects() {
+    return (
+      [
+        {
+          source: "/tomheaton",
+          destination: "api/qrcode/tomheaton",
+          permanent: true
+        }
+      ]
+    );
+  }
 }
