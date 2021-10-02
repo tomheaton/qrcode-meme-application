6ccccc32d1de34ef614e08b94e3221e30603997b
Thomas Heaton <thomas.m.heaton@gmail.com>
2021-10-02 22:23:09 +0100

    add new pointing emoji (👇)

--- client-next/src/pages/index.tsx
+++ client-next/src/pages/index.tsx
@@ -30,7 +30,8 @@ const Home: NextPage = () => {
                     <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
                         username
                     </code>
-                    {" "}below {/*⤵*/}️️
+                    {" "}below
+                    <br/>👇{/*⤵*/}️️
                 </p>
                 <br/>
                 <div onSubmit={handleSubmit} className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
