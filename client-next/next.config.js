503c32dcbc148515c2b15e69cce8c24a466f4844
Thomas Heaton <thomas.m.heaton@gmail.com>
2021-11-19 15:46:29 +0000

    more spaces

--- client-next/next.config.js
+++ client-next/next.config.js
@@ -1,16 +1,16 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
+    reactStrictMode: true,
 
-  async redirects() {
-    return (
-      [
-        {
-          source: "/tomheaton",
-          destination: "api/qrcode/tomheaton",
-          permanent: true
-        }
-      ]
-    );
-  }
+    async redirects() {
+        return (
+            [
+                {
+                    source: "/tomheaton",
+                    destination: "api/qrcode/tomheaton",
+                    permanent: true
+                }
+            ]
+        );
+    }
 }
