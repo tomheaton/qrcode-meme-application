e1967b4512616b2327a08466614d0acffdaa2810
Tom Heaton <thomas.m.heaton@gmail.com>
2023-02-18 17:48:36 +0000

    format code

--- client-old/src/components/FormComponent/index.js
+++ client-old/src/components/FormComponent/index.js
@@ -1,176 +1,196 @@
 import React from 'react';
-import { Container, Button, ProgressBar, Form, Row, Col, Accordion, Card } from 'react-bootstrap';
+import {Container, Button, ProgressBar, Form, Row, Col, Accordion, Card} from 'react-bootstrap';
 import "../../styles/styles.scss";
 import axios from "axios";
 
 export default class FormComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            responseToPost: "",
+            memes: [],
+            selectedMeme: "",
+            name: "",
+            url: "",
+            selectedMethod: "",
+            uploadMemeName: "",
+            uploadMemeURL: "",
+            selectedMemeToRemove: ""
+        };
 
-  constructor(props){
-    super(props);
-    this.state = {
-      responseToPost: "",
-      memes: [],
-      selectedMeme: "",
-      name: "",
-      url: "",
-      selectedMethod: "",
-      uploadMemeName: "",
-      uploadMemeURL: "",
-      selectedMemeToRemove: ""
+        this.handleSettingsSubmit = this.handleSettingsSubmit.bind(this);
+        this.handleUploadSubmit = this.handleUploadSubmit.bind(this);
+        this.handleRemoveEntry = this.handleRemoveEntry.bind(this);
+        this.handleRestoreDefaults = this.handleRestoreDefaults.bind(this);
+    }
+
+    componentDidMount() {
+        let initialMemes = [];
+        let initialMethod = "";
+        let initialMeme = "";
+        fetch('api/memes')
+            .then(res => res.json())
+            .then((data) => {
+                initialMemes = data["Memes"]["Videos"].map((meme) => {
+                    return meme.name
+                });
+                initialMethod = data["Memes"]["Method"];
+                initialMeme = data["Memes"]["ChosenMeme"][0].name;
+                this.setState({memes: initialMemes, selectedMethod: initialMethod, selectedMeme: initialMeme})
+            })
+            .catch(console.log)
+    }
+
+    handleSettingsSubmit = async () => {
+        const {selectedMeme, selectedMethod,} = this.state;
+        await axios.post("api/memes/settings", {name: selectedMeme, method: selectedMethod});
+        document.location.reload(true);
     };
 
-    this.handleSettingsSubmit = this.handleSettingsSubmit.bind(this);
-    this.handleUploadSubmit = this.handleUploadSubmit.bind(this);
-    this.handleRemoveEntry = this.handleRemoveEntry.bind(this);
-    this.handleRestoreDefaults = this.handleRestoreDefaults.bind(this);
-  }
+    handleUploadSubmit = async () => {
+        const {uploadMemeName, uploadMemeURL} = this.state;
+        await axios.post("api/memes", {name: uploadMemeName, url: uploadMemeURL});
+        document.location.reload(true);
+    };
 
-  componentDidMount() {
-    let initialMemes = [];
-    let initialMethod = "";
-    let initialMeme = "";
-    fetch('api/memes')
-      .then(res => res.json())
-      .then((data) => {
-        initialMemes = data["Memes"]["Videos"].map((meme) => { return meme.name });
-        initialMethod = data["Memes"]["Method"];
-        initialMeme = data["Memes"]["ChosenMeme"][0].name;
-        this.setState({ memes: initialMemes, selectedMethod: initialMethod, selectedMeme: initialMeme })
-      })
-      .catch(console.log)
-  }
+    handleRemoveEntry = async () => {
+        const {selectedMemeToRemove, memes} = this.state;
+        let selectedIndex = memes.indexOf(selectedMemeToRemove);
+        await axios.post("memes/remove", {name: selectedMemeToRemove, index: selectedIndex});
+        document.location.reload(true);
+    };
 
-  handleSettingsSubmit = async () => {
-    const { selectedMeme, selectedMethod, } = this.state;
-    await axios.post("api/memes/settings", {name: selectedMeme, method: selectedMethod});
-    document.location.reload(true);
-  };
+    handleRestoreDefaults = async () => {
+        await axios.post("api/memes/restore");
+        document.location.reload(true);
+    };
 
-  handleUploadSubmit = async () => {
-    const { uploadMemeName, uploadMemeURL } = this.state;
-    await axios.post("api/memes", {name: uploadMemeName, url: uploadMemeURL});
-    document.location.reload(true);
-  };
+    render() {
+        const {memes, selectedMethod, selectedMemeToRemove, uploadMemeName, uploadMemeURL, selectedMeme} = this.state;
 
-  handleRemoveEntry = async () => {
-    const { selectedMemeToRemove, memes } = this.state;
-    let selectedIndex = memes.indexOf(selectedMemeToRemove);
-    await axios.post("memes/remove", { name: selectedMemeToRemove, index: selectedIndex });
-    document.location.reload(true);
-  };
-
-  handleRestoreDefaults = async () => {
-    await axios.post("api/memes/restore");
-    document.location.reload(true);
-  };
-
-  render() {
-    const { memes, selectedMethod, selectedMemeToRemove, uploadMemeName, uploadMemeURL, selectedMeme } = this.state;
-
-    return (
-      <Container>
-        <h1>QR Code Meme Manager</h1>
-        <br/>
-        <ProgressBar variant={"success"} min={0} max={5} now={5}/>
-        <br/>
-
-        <Accordion>
-
-          <Card>
-            <Accordion.Toggle as={Card.Header} eventKey="0"> <h6>Meme Settings</h6> </Accordion.Toggle>
-            <Accordion.Collapse eventKey="0">
-              <Card.Body>
-                <Form>
-                  <Form.Group as={Row} controlId="formHorizontalEmail">
-                    <Form.Label column sm={2}> Method Selector: </Form.Label>
-                    <Col sm={10}>
-                      <select value={selectedMethod } onChange={e => this.setState({ selectedMethod: e.target.value })} className={"custom-select"}>
-                        <option selected disabled> Select a method for displaying memes. </option>
-                        <option value={"selected"}> Selected Meme </option>
-                        <option value={"random"}> Random Meme </option>
-                      </select>
-                    </Col>
-                  </Form.Group>
-                  <br/>
-                  {(selectedMethod === "selected") && (
-                    <Form.Group as={Row} controlId="formHorizontalEmail">
-                    <Form.Label column sm={2}> Meme Selector: </Form.Label>
-                    <Col sm={10}>
-                      <select value={selectedMeme} onChange={e => this.setState({ selectedMeme: e.target.value })} className={"custom-select"}>
-                        <option selected disabled> Select a meme to display. </option>
-                        { memes.map((meme) => { return <option key={meme}> {meme} </option> })}
-                      </select>
-                    </Col>
-                  </Form.Group>)}
-
-                </Form>
+        return (
+            <Container>
+                <h1>QR Code Meme Manager</h1>
                 <br/>
-                <Button onClick={this.handleSettingsSubmit} variant={"success"} block> Submit </Button>
-              </Card.Body>
-            </Accordion.Collapse>
-          </Card>
-
-          <Card>
-            <Accordion.Toggle as={Card.Header} eventKey="1"> <h6>Meme Upload</h6> </Accordion.Toggle>
-            <Accordion.Collapse eventKey="1">
-              <Card.Body>
-                <Form>
-                  <Form.Group as={Row} controlId="formHorizontalEmail">
-                    <Form.Label column sm={2}> Meme Name: </Form.Label>
-                    <Col sm={10}>
-                      <input type={"text"} className={"form-control"} onChange={e => this.setState({ uploadMemeName: e.target.value })} placeholder={"Enter a name for the meme."} />
-                    </Col>
-                  </Form.Group>
-                  <br/>
-                  <Form.Group as={Row} controlId="formHorizontalEmail">
-                    <Form.Label column sm={2}> Meme URL: </Form.Label>
-                    <Col sm={10}>
-                      <input type={"text"} className={"form-control"} onChange={e => this.setState({ uploadMemeURL: e.target.value })} placeholder={"Enter the URL for the meme."} />
-                    </Col>
-                  </Form.Group>
-                </Form>
+                <ProgressBar variant={"success"} min={0} max={5} now={5}/>
                 <br/>
-                {(uploadMemeName && uploadMemeURL) && (<Button onClick={this.handleUploadSubmit} variant={"success"} block> Upload </Button>)}
-              </Card.Body>
-            </Accordion.Collapse>
-          </Card>
 
-          <Card>
-            <Accordion.Toggle as={Card.Header} eventKey="2"> <h6>Meme Remover</h6> </Accordion.Toggle>
-            <Accordion.Collapse eventKey="2">
-              <Card.Body>
-                <Form>
-                  <Form.Group as={Row} controlId="formHorizontalEmail">
-                    <Form.Label column sm={2}> Meme Selector: </Form.Label>
-                    <Col sm={10}>
-                      <select defaultValue={1} value={selectedMemeToRemove} onChange={e => this.setState({ selectedMemeToRemove: e.target.value })} className={"custom-select"}>
-                        <option value={1}> Select a meme to remove. </option>
-                        { memes.map((meme) => { return <option key={meme}> {meme} </option> })}
-                      </select>
-                    </Col>
-                  </Form.Group>
-                </Form>
+                <Accordion>
+
+                    <Card>
+                        <Accordion.Toggle as={Card.Header} eventKey="0"><h6>Meme Settings</h6></Accordion.Toggle>
+                        <Accordion.Collapse eventKey="0">
+                            <Card.Body>
+                                <Form>
+                                    <Form.Group as={Row} controlId="formHorizontalEmail">
+                                        <Form.Label column sm={2}> Method Selector: </Form.Label>
+                                        <Col sm={10}>
+                                            <select value={selectedMethod}
+                                                    onChange={e => this.setState({selectedMethod: e.target.value})}
+                                                    className={"custom-select"}>
+                                                <option selected disabled> Select a method for displaying memes.
+                                                </option>
+                                                <option value={"selected"}> Selected Meme</option>
+                                                <option value={"random"}> Random Meme</option>
+                                            </select>
+                                        </Col>
+                                    </Form.Group>
+                                    <br/>
+                                    {(selectedMethod === "selected") && (
+                                        <Form.Group as={Row} controlId="formHorizontalEmail">
+                                            <Form.Label column sm={2}> Meme Selector: </Form.Label>
+                                            <Col sm={10}>
+                                                <select value={selectedMeme}
+                                                        onChange={e => this.setState({selectedMeme: e.target.value})}
+                                                        className={"custom-select"}>
+                                                    <option selected disabled> Select a meme to display.</option>
+                                                    {memes.map((meme) => {
+                                                        return <option key={meme}> {meme} </option>
+                                                    })}
+                                                </select>
+                                            </Col>
+                                        </Form.Group>)}
+
+                                </Form>
+                                <br/>
+                                <Button onClick={this.handleSettingsSubmit} variant={"success"} block> Submit </Button>
+                            </Card.Body>
+                        </Accordion.Collapse>
+                    </Card>
+
+                    <Card>
+                        <Accordion.Toggle as={Card.Header} eventKey="1"><h6>Meme Upload</h6></Accordion.Toggle>
+                        <Accordion.Collapse eventKey="1">
+                            <Card.Body>
+                                <Form>
+                                    <Form.Group as={Row} controlId="formHorizontalEmail">
+                                        <Form.Label column sm={2}> Meme Name: </Form.Label>
+                                        <Col sm={10}>
+                                            <input type={"text"} className={"form-control"}
+                                                   onChange={e => this.setState({uploadMemeName: e.target.value})}
+                                                   placeholder={"Enter a name for the meme."}/>
+                                        </Col>
+                                    </Form.Group>
+                                    <br/>
+                                    <Form.Group as={Row} controlId="formHorizontalEmail">
+                                        <Form.Label column sm={2}> Meme URL: </Form.Label>
+                                        <Col sm={10}>
+                                            <input type={"text"} className={"form-control"}
+                                                   onChange={e => this.setState({uploadMemeURL: e.target.value})}
+                                                   placeholder={"Enter the URL for the meme."}/>
+                                        </Col>
+                                    </Form.Group>
+                                </Form>
+                                <br/>
+                                {(uploadMemeName && uploadMemeURL) && (
+                                    <Button onClick={this.handleUploadSubmit} variant={"success"}
+                                            block> Upload </Button>)}
+                            </Card.Body>
+                        </Accordion.Collapse>
+                    </Card>
+
+                    <Card>
+                        <Accordion.Toggle as={Card.Header} eventKey="2"><h6>Meme Remover</h6></Accordion.Toggle>
+                        <Accordion.Collapse eventKey="2">
+                            <Card.Body>
+                                <Form>
+                                    <Form.Group as={Row} controlId="formHorizontalEmail">
+                                        <Form.Label column sm={2}> Meme Selector: </Form.Label>
+                                        <Col sm={10}>
+                                            <select defaultValue={1} value={selectedMemeToRemove}
+                                                    onChange={e => this.setState({selectedMemeToRemove: e.target.value})}
+                                                    className={"custom-select"}>
+                                                <option value={1}> Select a meme to remove.</option>
+                                                {memes.map((meme) => {
+                                                    return <option key={meme}> {meme} </option>
+                                                })}
+                                            </select>
+                                        </Col>
+                                    </Form.Group>
+                                </Form>
+                                <br/>
+                                {(selectedMemeToRemove) && (
+                                    <Button onClick={this.handleRemoveEntry} variant={"success"} block> Remove
+                                        Entry </Button>)}
+                                <br/>
+                            </Card.Body>
+                        </Accordion.Collapse>
+                    </Card>
+
+                    <Card>
+                        <Accordion.Toggle as={Card.Header} eventKey="3"><h6>About</h6></Accordion.Toggle>
+                        <Accordion.Collapse eventKey="3">
+                            <Card.Body>
+                                <h4 className={"text-center"}> Tom Heaton | 2020 </h4>
+                            </Card.Body>
+                        </Accordion.Collapse>
+                    </Card>
+
+                </Accordion>
                 <br/>
-                {(selectedMemeToRemove) && (<Button onClick={this.handleRemoveEntry} variant={"success"} block> Remove Entry </Button>)}
-                <br/>
-              </Card.Body>
-            </Accordion.Collapse>
-          </Card>
+                <Button onClick={this.handleRestoreDefaults} variant={"success"} block> Restore To Defaults </Button>
 
-          <Card>
-            <Accordion.Toggle as={Card.Header} eventKey="3"> <h6>About</h6> </Accordion.Toggle>
-            <Accordion.Collapse eventKey="3">
-              <Card.Body>
-                <h4 className={"text-center"}> Tom Heaton | 2020 </h4>
-              </Card.Body>
-            </Accordion.Collapse>
-          </Card>
-
-        </Accordion>
-        <br/>
-        <Button onClick={this.handleRestoreDefaults} variant={"success"} block> Restore To Defaults </Button>
-
-      </Container>
-    );
-  }
+            </Container>
+        );
+    }
 }
--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+
 const express = require('express');
 const lodash = require("lodash");
 const cors = require('cors');
@@ -6,114 +7,113 @@ const fs = require("fs");
 
 const app = express();
 const port = process.env.PORT || 5000;
+
 app.use(express.json());
-app.use(cors.config({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors.config({origin: "http://localhost:3000", credentials: true}));
 
 const restoreDefaults = async () => {
-  fs.readFile("../defaults.json", 'utf8', (err, data) => {
-    fs.writeFile('../data.json', data, (err) => {
-      let timestamp = new Date();
-      if (err) return console.error(err);
-      console.log(`--> Meme database successfully restored to defaults. - ${timestamp.toLocaleTimeString()}`);
+    fs.readFile("../defaults.json", 'utf8', (err, data) => {
+        fs.writeFile('../data.json', data, (err) => {
+            let timestamp = new Date();
+            if (err) return console.error(err);
+            console.log(`--> Meme database successfully restored to defaults. - ${timestamp.toLocaleTimeString()}`);
+        })
     })
-  })
 };
 
 const removeEntry = async (name, index) => {
-  fs.readFile("../data.json", 'utf8', (err, data) => {
+    fs.readFile("../data.json", 'utf8', (err, data) => {
 
-    let memeData = JSON.parse(data);
-    if (index > -1) {
-      memeData["memes"]["videos"].splice(index, 1);
-    }
-    let string = JSON.stringify(memeData, null, '\t');
+        let memeData = JSON.parse(data);
+        if (index > -1) {
+            memeData["memes"]["videos"].splice(index, 1);
+        }
+        let string = JSON.stringify(memeData, null, '\t');
 
-    fs.writeFile('./data.json', string, (err) => {
-      let timestamp = new Date();
-      if (err) return console.error(err);
-      console.log(`--> Meme successfully removed from JSON File. - ${timestamp.toLocaleTimeString()}`);
+        fs.writeFile('./data.json', string, (err) => {
+            let timestamp = new Date();
+            if (err) return console.error(err);
+            console.log(`--> Meme successfully removed from JSON File. - ${timestamp.toLocaleTimeString()}`);
+        })
     })
-  })
 };
 
 const saveMemeToJSON = async (meme) => {
-  fs.readFile("../data.json", 'utf8', (err, data) => {
+    fs.readFile("../data.json", 'utf8', (err, data) => {
 
-    let memeData = JSON.parse(data);
-    memeData["memes"]["videos"].push(meme);
-    let string = JSON.stringify(memeData, null, '\t');
+        let memeData = JSON.parse(data);
+        memeData["memes"]["videos"].push(meme);
+        let string = JSON.stringify(memeData, null, '\t');
 
-    fs.writeFile('../data.json', string, (err) => {
-      let timestamp = new Date();
-      if (err) return console.error(err);
-      console.log(`--> Meme successfully added to JSON file. - ${timestamp.toLocaleTimeString()}`);
+        fs.writeFile('../data.json', string, (err) => {
+            let timestamp = new Date();
+            if (err) return console.error(err);
+            console.log(`--> Meme successfully added to JSON file. - ${timestamp.toLocaleTimeString()}`);
+        })
     })
-  })
 };
 
 const saveSettingsToJSON = async (name, method) => {
-  fs.readFile("./data.json", 'utf8', (err, data) => {
+    fs.readFile("./data.json", 'utf8', (err, data) => {
 
-    let memeData = JSON.parse(data);
-    let chosenMeme = lodash.filter(memeData["memes"]["videos"], x => x.name === name);
+        let memeData = JSON.parse(data);
+        memeData["memes"]["chosenMeme"] = lodash.filter(memeData["memes"]["videos"], x => x.name === name);
+        memeData["memes"]["method"] = method;
 
-    memeData["memes"]["chosenMeme"] = chosenMeme;
-    memeData["memes"]["method"] = method;
+        let string = JSON.stringify(memeData, null, '\t');
 
-    let string = JSON.stringify(memeData, null, '\t');
-
-    fs.writeFile('./data.json', string, (err) => {
-      let timestamp = new Date();
-      if (err) return console.error(err);
-      console.log(`--> Settings successfully updated and added to JSON file. - ${timestamp.toLocaleTimeString()}`);
+        fs.writeFile('./data.json', string, (err) => {
+            let timestamp = new Date();
+            if (err) return console.error(err);
+            console.log(`--> Settings successfully updated and added to JSON file. - ${timestamp.toLocaleTimeString()}`);
+        })
     })
-  })
 };
 
 app.post('/api/memes', async (req, res) => {
-  let response = await saveMemeToJSON(req.body);
-  res.send("Your meme has been added.");
+    let response = await saveMemeToJSON(req.body);
+    res.send("Your meme has been added.");
 });
 
 app.post('/api/memes/restore', async (req, res) => {
-  let response = await restoreDefaults();
-  res.send("Meme database has been Restored To Defaults.");
+    let response = await restoreDefaults();
+    res.send("Meme database has been Restored To Defaults.");
 });
 
 app.post('/api/memes/remove', async (req, res) => {
-  let response = await removeEntry(req.body.name, req.body.index);
-  res.send("Meme removed from Database.");
+    let response = await removeEntry(req.body.name, req.body.index);
+    res.send("Meme removed from Database.");
 });
 
 app.post('/api/memes/settings', async (req, res) => {
-  let response = await saveSettingsToJSON(req.body.name, req.body.method);
-  res.send("Settings Updated.");
+    let response = await saveSettingsToJSON(req.body.name, req.body.method);
+    res.send("Settings Updated.");
 });
 
 app.get('/api/memes', async (req, res) => {
-  fs.readFile("./data.json", 'utf8', (err, data) => {
-    if (err) return console.error(err);
-    res.send(JSON.parse(data));
-  });
+    fs.readFile("./data.json", 'utf8', (err, data) => {
+        if (err) return console.error(err);
+        res.send(JSON.parse(data));
+    });
 });
 
 app.get('/api/meme', async (req, res) => {
-  fs.readFile("./data.json", 'utf8', (err, data) => {
-    if (err) return console.error(err);
-    let meme = {};
-    let memeData = JSON.parse(data);
-    let memeList = memeData["memes"]["videos"];
-    if (memeData["memes"]["method"] === "random") {
-      meme = memeList[Math.floor(Math.random() * memeList.length)];
-    } else {
-      meme = memeData["memes"]["chosenMeme"][0];
-    }
+    fs.readFile("./data.json", 'utf8', (err, data) => {
+        if (err) return console.error(err);
+        let meme = {};
+        let memeData = JSON.parse(data);
+        let memeList = memeData["memes"]["videos"];
+        if (memeData["memes"]["method"] === "random") {
+            meme = memeList[Math.floor(Math.random() * memeList.length)];
+        } else {
+            meme = memeData["memes"]["chosenMeme"][0];
+        }
 
-    //res.send(meme);
-    res.statusCode = 302; // 301 = permanent redirect, 302 = temporary redirect.
-    res.setHeader("Location", meme.url);
-    res.end();
-  });
+        //res.send(meme);
+        res.statusCode = 302; // 301 = permanent redirect, 302 = temporary redirect.
+        res.setHeader("Location", meme.url);
+        res.end();
+    });
 });
 
 app.listen(port, () => console.log(`QR Code Server listening on port ${port}`));
